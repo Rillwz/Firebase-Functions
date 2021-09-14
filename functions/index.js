c3e3edec28398cb7d1b5fd0332602703d1de43e1
Yusril Ihsanul Alim <73714271+Rillwz@users.noreply.github.com>
2021-09-15 06:52:55 +0700

    Auth trigger collection doc

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,21 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
 
 // auth trigger ( new user signup )
 exports.newUserSignup = functions.auth.user().onCreate((user) => {
   console.log("user created", user.email, user.uid);
+  return admin.firestore().collection("users").doc(user.uid).set({
+    email: user.email,
+    upvotedOn: [],
+  });
+  // for background triggers you must return a valur/promise
 });
 
 // auth trigger ( new user signup )
 exports.userDeleted = functions.auth.user().onDelete((user) => {
   console.log("user deleted", user.email, user.uid);
+  // for background triggers you must return a value/promise
+  const doc = admin.firestore().collection("users").doc(user.uid);
+  return doc.delete();
 });
